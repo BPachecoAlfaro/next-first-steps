1ac02f1b79d78cb317d9cd69342dfa42f2452dd0
bpachecoalfaro <bpachecoalfaro@gmail.com>
2024-12-05 08:49:34 -0300

    First commit

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: 'Contact Page',
+	description: 'Contact services',
+};
+
+
+export default function ContactPage() {
+
+	return (
+		<>
+			<span className="text-7xl">Contact Page</span>
+		</>
+	)
+}
